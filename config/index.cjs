ec2dc982ab6e68a7ce7e15bc425185f24409fe6f
Calvin <41325875+Calvin9982@users.noreply.github.com>
2025-09-07 12:28:09 +0800

    Update index.cjs
    13

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohsj314VBzogiIINziyJtZWFijyA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'FnF1gy5hQZoNgNpBAHSKVPGDKlnPbf1hKLNCA3Clu_c',
+      useTemplateId: 'TtefVhXKBTAM_bZUQ5XLaQQ_fz2dTDdZVf_RuAKnmds',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-06',
       festivals: [
