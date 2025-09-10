404d5e2987cf29df6a3acee3da44b70f1449de72
Calvin <41325875+Calvin9982@users.noreply.github.com>
2025-09-10 21:55:48 +0800

    Update index.cjs
    13

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '萱兮',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ohsj314VBzogiIINziyJtZWFijyA',
+      id: 'ohsj318D3d1NUJcj0lRbE21XiY94',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'k_99jvQvw-8ktxzHMXa6MFuaLeA48OO6__5qXqTFOEM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
